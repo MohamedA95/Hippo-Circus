d5ee9e1d46e1b4de536e617ab444b5ba56802c12
habi3000 <habi30002009@gmail.com>
2016-02-04 23:54:14 +0300

    version2

--- src/hippo/Hippo.java
+++ src/hippo/Hippo.java
@@ -9,43 +9,37 @@ public class Hippo {
 
     public static void main(String[] args) {
         Scanner input = null;
-        try {
-            input = new Scanner(Paths.get("hippo.in"));
-        } catch (IOException io) {
-            System.out.println("io");
-        }
+        try {input = new Scanner(Paths.get("hippo.in"));}
+        catch (IOException io) {System.out.println("io");}
         int t = input.nextInt();
         for (int i = 1; i <= t; i++) {
             //geting data
             int n = input.nextInt(), dh = input.nextInt(), ta = input.nextInt(), td = input.nextInt();
+                        int dhippo = 0, shippo = 0;
+
             int[] hippos = new int[n];
-            //filling arry
-            for (int q = 0; q < n; q++) {
-                hippos[q] = input.nextInt();
-            }
-            //sorting arry
-            Arrays.sort(hippos);
+            //filling array
+            for (int q = 0; q < n; q++) {hippos[q] = input.nextInt();}
+            //sorting array
+           Arrays.parallelSort(hippos);
+           //counting 
             for (int w = 0; w < hippos.length; w++) {
                 for (int j = 0; j < hippos.length; j++) {
                     int t1 = hippos[w], t2 = hippos[j];
                     if (t1 + t2 < dh && w != j && t1 != -1 && t2 != -1) {
                         hippos[w] = -1;
                         hippos[j] = -1;
+                        dhippo++;
 
                     }
                 }
             }
-            int dhippo = 0, shippo = 0;
             for (int e = 0; e < hippos.length; e++) {
-                if (hippos[e] == -1) {
-                    dhippo++;
-                } else {
+                if (hippos[e] != -1)
                     shippo++;
-                }
 
             }
-            int time = (dhippo / 2) * td + shippo * ta;
-            System.out.println("Case " + i + ": " + (time));
+            System.out.println("Case " + i + ": " + (dhippo * td + shippo * ta));
 
         }
 
